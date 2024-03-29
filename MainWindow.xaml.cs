360afd5ff37679755b7e8b88964d8cf8c676f45e
Alex <aleksandr.tsepkovskii@edu.karelia.fi>
2024-03-29 16:53:16 +0200

    Revert "added array generation"
    This reverts commit 8e420523e34633a42543248d0bb887e437c92c30.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,48 +20,15 @@ namespace Wpf_Karelia
     public partial class MainWindow : Window
     {
         Grid gridMain;
-        int[,] minesArray;
+
         public MainWindow()
         {
             InitializeComponent();
 
             //initializeArray (2 dimensional) - random b + neighbors
-            minesArray = CreateMinesArray(10, 10);
-
             DrawGrid();
             this.Content = gridMain;
 
-        }
-
-        private int[,] CreateMinesArray(int y, int x)
-        {
-            int [,] array = new int[y, x];
-            int minesCount = y * x / 10;
-            Random rnd = new Random();
-            for (int c = 0; c < minesCount; c++)
-            {
-                array[rnd.Next(y), rnd.Next(x)] = -1;
-            }
-            for (int i = 0; i < y; i++)
-            {
-                for (int j = 0; j < x; j++)
-                {
-                    if (array[i, j] == -1)
-                    {
-                        for (int k = -1; k < 2; k++)
-                        {
-                            for (int l = -1; l < 2; l++)
-                            {
-                                if ((0 <= i + k && i + k < y) && (0 <= j + l && j + l < x))
-                                { 
-                                    if (array[i + k, j + l] != -1) { array[i + k, j + l]++; }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return array;
         }
 
         private void DrawGrid()
@@ -70,15 +37,15 @@ namespace Wpf_Karelia
             gridMain = new Grid();
 
             gridMain.ShowGridLines = true;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 gridMain.ColumnDefinitions.Add(new ColumnDefinition());
                 gridMain.RowDefinitions.Add(new RowDefinition());
             }
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < 5; j++)
                 {
                     Button button = AddButton();
                     Grid.SetColumn(button, i);
@@ -132,7 +99,7 @@ namespace Wpf_Karelia
             int _column = (int)btn.GetValue(Grid.ColumnProperty);
 
             //btn.Content = drawCell(btn, _row, _column);
-            btn.Content = _row + " " + _column + "\n" + minesArray[_row, _column];
+            btn.Content = _row + " " + _column;
             btn.Click -= btnToggleRun_Click;
             btn.MouseRightButtonDown -= btnSigned;
 
